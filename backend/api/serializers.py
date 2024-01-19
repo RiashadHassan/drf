a05bb5a537213332724ffcc94d58bbec6e31acbe
Riashad Hassan <109424665+RiashadHassan@users.noreply.github.com>
2024-01-19 11:17:29 +0600

    MovieSerializer Object Validated

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -16,12 +16,42 @@ class MovieSerializer(serializers.ModelSerializer):
         instance.save()
         return instance
     
-    def validate_name(self, value):
+    # def validate_name(self, value):
         
-        if len(value) > 2:
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short")
+        
+    #     if Movie.objects.filter(name=value).exists():
+    #         raise serializers.ValidationError("Name already exists")
+        
+    #     return value
+    # def validate_description(self, value):
+        
+    #     if len(value) < 10:
+    #         raise serializers.ValidationError("Description is too short")
+        
+    #     if Movie.objects.filter(description=value).exists():
+    #         raise serializers.ValidationError("Description already exists")
+        
+    #     return value
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Description and name cannot be the same") 
+               
+        #name validation
+        if len(data['name']) < 2:
             raise serializers.ValidationError("Name is too short")
         
-        if Movie.objects.filter(name=value).exists():
+        if Movie.objects.filter(name=data['name']).exists():
             raise serializers.ValidationError("Name already exists")
         
-        return value
+        #description validation
+        if len(data['description']) < 10:
+            raise serializers.ValidationError("Description is too short")
+        
+        if Movie.objects.filter(description=data['description']).exists():
+            raise serializers.ValidationError("Description already exists")
+        
+        return data
+        
